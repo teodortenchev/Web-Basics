0735af526577fe0fe9a13cf80a159bb557dcf28d
Teodor Tenchev <teodortenchev@gmail.com>
2019-05-20 18:00:18 +0400

    Create HomeController.cs

---
+++ CustomMVC/SIS/SIS.Demo/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using SIS.HTTP.Enums;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.WebServer.Results;
+
+namespace SIS.Demo
+{
+    public class HomeController
+    {
+        public IHttpResponse Index(IHttpRequest request)
+        {
+            string content = "<h1>I pray to the powers to be that this works after so many days of coding!</h1>";
+
+            return new HtmlResult(content, HttpResponseStatusCode.OK);
+        }
+    }
+}
