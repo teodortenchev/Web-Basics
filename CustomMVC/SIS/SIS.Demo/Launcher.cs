68cb9c76821cbf37c7a525362e3bbb5672bc37cc
Teodor Tenchev <teodortenchev@gmail.com>
2019-05-20 17:59:59 +0400

    Create Launcher.cs

---
+++ CustomMVC/SIS/SIS.Demo/Launcher.cs
@@ -0,0 +1,24 @@
+﻿using SIS.HTTP.Enums;
+using SIS.WebServer;
+using SIS.WebServer.Routing;
+using System;
+
+namespace SIS.Demo
+{
+    public class Launcher
+    {
+        static void Main(string[] args)
+        {
+            IServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Add(
+                HttpRequestMethod.Get,
+                path: "/",
+                func: request => new HomeController().Index(request));
+
+            Server server = new Server(port: 8000, serverRoutingTable);
+
+            server.Run();
+        }
+    }
+}
