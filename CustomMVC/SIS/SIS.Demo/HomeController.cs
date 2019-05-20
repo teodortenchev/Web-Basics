7ea61dea547d35efad3407cc9f11d3e1ccc9925a
Teodor Tenchev <teodortenchev@gmail.com>
2019-05-20 18:21:00 +0400

    Now returning an  html page

--- CustomMVC/SIS/SIS.Demo/HomeController.cs
+++ CustomMVC/SIS/SIS.Demo/HomeController.cs
@@ -2,14 +2,16 @@
 using SIS.HTTP.Requests;
 using SIS.HTTP.Responses;
 using SIS.WebServer.Results;
+using System.IO;
 
 namespace SIS.Demo
 {
     public class HomeController
     {
+        private const string indexPath = @"E:\C# Fundamentals\Web Basics\code\Web-Basics\CustomMVC\SIS\SIS.Demo\Pages\index.html";
         public IHttpResponse Index(IHttpRequest request)
         {
-            string content = "<h1>I pray to the powers to be that this works after so many days of coding!</h1>";
+            string content = File.ReadAllText(indexPath);
 
             return new HtmlResult(content, HttpResponseStatusCode.OK);
         }
