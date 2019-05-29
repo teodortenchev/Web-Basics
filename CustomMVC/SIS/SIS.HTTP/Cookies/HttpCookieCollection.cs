58b25a9e792aec3a62bd059deaf7c72826aee2f1
Teodor Tenchev <teodortenchev@gmail.com>
2019-05-29 20:45:51 +0400

    Create HttpCookieCollection.cs

---
+++ CustomMVC/SIS/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Cookies
+{
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private Dictionary<string, HttpCookie> cookies;
+
+        public HttpCookieCollection()
+        {
+            cookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasCookies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
