bda50203175df944a2f49273fad73990d9cbf717
Teodor Tenchev <teodortenchev@gmail.com>
2019-05-29 21:27:34 +0400

    Update HttpCookieCollection.cs

--- CustomMVC/SIS/SIS.HTTP/Cookies/HttpCookieCollection.cs
+++ CustomMVC/SIS/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SIS.HTTP.Common;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,8 @@ namespace SIS.HTTP.Cookies
 {
     public class HttpCookieCollection : IHttpCookieCollection
     {
+        
+
         private Dictionary<string, HttpCookie> cookies;
 
         public HttpCookieCollection()
@@ -15,22 +19,55 @@ namespace SIS.HTTP.Cookies
 
         public void AddCookie(HttpCookie cookie)
         {
-            throw new NotImplementedException();
+            CoreValidator.ThrowIfNull(cookie, nameof(cookie));
+
+            cookies.Add(cookie.Key, cookie);
         }
 
         public bool ContainsCookie(string key)
         {
-            throw new NotImplementedException();
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+            return cookies.ContainsKey(key);
         }
 
         public HttpCookie GetCookie(string key)
         {
-            throw new NotImplementedException();
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            //TO DO: Check if this is the correct behavior.
+            if (cookies.ContainsKey(key))
+            {
+                return cookies[key];
+            }
+
+            return null;
         }
 
         public bool HasCookies()
         {
-            throw new NotImplementedException();
+            return cookies.Count != 0;
+        }
+
+        public IEnumerator<HttpCookie> GetEnumerator()
+        {
+            return cookies.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var cookie in cookies.Values)
+            {
+                sb.Append($"Set-Cookie: {cookie}").Append(GlobalConstants.HTTPNewLine);
+            }
+
+            return sb.ToString();
         }
     }
 }
