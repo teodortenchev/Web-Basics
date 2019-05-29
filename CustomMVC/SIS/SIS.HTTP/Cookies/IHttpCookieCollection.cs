bdc9b975f763c9093b3bb90bc94e4e6a193fd6ca
Teodor Tenchev <teodortenchev@gmail.com>
2019-05-29 20:45:57 +0400

    Create IHttpCookieCollection.cs

---
+++ CustomMVC/SIS/SIS.HTTP/Cookies/IHttpCookieCollection.cs
@@ -0,0 +1,10 @@
+﻿namespace SIS.HTTP.Cookies
+{
+    public interface IHttpCookieCollection
+    {
+        void AddCookie(HttpCookie cookie);
+        bool ContainsCookie(string key);
+        HttpCookie GetCookie(string key);
+        bool HasCookies();
+    }
+}
