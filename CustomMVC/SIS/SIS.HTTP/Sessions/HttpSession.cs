47c391461626b29d59ac818e2a2bcd4d395259e5
Teodor Tenchev <teodortenchev@gmail.com>
2019-06-01 12:20:30 +0400

    Create HttpSession.cs

---
+++ CustomMVC/SIS/SIS.HTTP/Sessions/HttpSession.cs
@@ -0,0 +1,54 @@
+﻿using SIS.HTTP.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Sessions
+{
+    public class HttpSession : IHttpSession
+    {
+        private Dictionary<string, object> sessionParameters;
+        public string Id { get; }
+
+        public HttpSession(string id)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(id, nameof(id));
+
+            Id = id;
+            sessionParameters = new Dictionary<string, object>();
+        }
+
+
+        public void AddParameter(string name, object parameter)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(name, nameof(name));
+            CoreValidator.ThrowIfNull(parameter, nameof(parameter));
+
+            sessionParameters["name"] = parameter;
+        }
+
+        public void ClearParameters()
+        {
+            sessionParameters.Clear();
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(name, nameof(name));
+
+            return sessionParameters.ContainsKey(name);
+        }
+
+        public object GetParameter(string name)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(name, nameof(name));
+
+            if(sessionParameters.ContainsKey(name))
+            {
+                return sessionParameters[name];
+            }
+
+            throw new InvalidOperationException("Session does not exist: " + name);
+        }
+    }
+}
