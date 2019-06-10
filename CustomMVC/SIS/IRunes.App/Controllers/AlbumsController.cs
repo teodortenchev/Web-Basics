7ddcd76a1cf28f22caf8b8c242ddeb2c93f9ac98
Teodor Tenchev <teodortenchev@gmail.com>
2019-06-10 23:49:21 +0400

    Create AlbumsController.cs

---
+++ CustomMVC/SIS/IRunes.App/Controllers/AlbumsController.cs
@@ -0,0 +1,18 @@
+﻿using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses.Contracts;
+
+namespace IRunes.App.Controllers
+{
+    public class AlbumsController : BaseController
+    {
+        public IHttpResponse AlbumView(IHttpRequest httpRequest)
+        {
+            if(IsLoggedIn(httpRequest))
+            {
+                return View("All");
+            }
+
+            return Redirect("/Users/Login");
+        }
+    }
+}
