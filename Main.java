a7eedd5998d4ca16f7c4ee3cb9c94cac94ca1beb
Teodor Tenchev <teodortenchev@gmail.com>
2021-01-07 17:51:40 +0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[] = (int[]) Array.newInstance(int.class, 5); // a new instance is created using Array.newInstance method.
+        Array.set(arr, 0, 2); // We set the values of the array with Array.set() method.
+        Array.set(arr, 2, 2);
+        Array.set(arr, 1, 9);
+        Array.set(arr, 2, 3);
+        Array.set(arr, 4, 7);
+        System.out.print("The array elements are: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+
+        }
+    }
+}
